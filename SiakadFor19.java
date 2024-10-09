067fb7c735575cf44190978c4d4604fdf87acb3c
MNawfal24 <m.nawfal.mawla@gmail.com>
2024-10-09 20:18:07 +0700

    Menambahkan program lulus / tidak lulus

--- SiakadFor19.java
+++ SiakadFor19.java
@@ -3,6 +3,7 @@ public class SiakadFor19{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, terendah = 100, tertinggi = 0;
+        int lulus = 0, tdkLulus = 0;
         
         for (int i = 1; i <=10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -13,8 +14,15 @@ public class SiakadFor19{
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai < 60) {
+                tdkLulus++;
+            } else {
+                lulus++;
+            }
         }
         System.out.println(" Nilai tertinggi : " + tertinggi);
         System.out.println("Nilai Terendah : " + terendah);
+        System.out.println(" Jumlah mahaiswa lulus = " + lulus);
+        System.out.println(" Jumlah mahasiswa tidak lulus = " + tdkLulus);
     }
 }
