544dbd673f4fd4125a69a92d9f170c5afea13d7d
MNawfal24 <m.nawfal.mawla@gmail.com>
2024-10-10 10:08:20 +0700

    commit kafeDoWhile

--- KafeDoWhile19.java
+++ KafeDoWhile19.java
@@ -2,5 +2,26 @@ import java.util.Scanner;
 public class KafeDoWhile19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+        int hargakopi = 12000, hargateh = 7000, hargaroti = 20000, totalHarga, kopi, teh, roti;
+        String namaPelanggan;
+        do {
+            System.out.println(" Masukkan nama pelanggan (ketik 'batal' untuk keluar): ");
+            namaPelanggan = sc.nextLine();
+            if(namaPelanggan.equalsIgnoreCase("batal")) {
+                System.out.println("Transaksi dibatalkan." );
+                break;
+            }
+            System.out.print("jumlah kopi: ");
+            kopi = sc.nextInt();
+            System.out.print("jumlah teh: ");
+            teh = sc.nextInt();
+            System.out.print("jumlah roti : ");
+            roti = sc.nextInt();
+            totalHarga = (kopi * hargakopi) + (teh * hargateh) + (roti * hargaroti);
+            System.out.println(" total yang hars dibayar : Rp " + totalHarga);
+            sc.nextLine();
+        } while (true);
+
+        System.out.println(" Semua Transaksi Selesai ");
     }
 }
